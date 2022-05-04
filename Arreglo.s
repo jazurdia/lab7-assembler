2cefaff35c37d2207532a4c6afbe2b803c4c00a7
danicabrera <69865919+danicabrera@users.noreply.github.com>
2022-05-04 12:56:11 -0600

    100% Funcional

--- Arreglo.s
+++ Arreglo.s
@@ -59,15 +59,15 @@ formatoMenu: .asciz "Menu: \n1. Leche\n2. P. Galletas\n3. Mantequilla\n4. Queso\
 
 formatoSalida1: .asciz "Nombre del cliente: %s \n"
 formatoSalida2: .asciz "Producto		Precio		Cantidad		SubTotal\n"
-formatoLeche: .asciz "Leche				Q.18			%d				"
-formatoGalleta: .asciz "\nGalletas				Q.25			%d				"
-formatoMantequilla: .asciz "\nMantequilla				Q.10		%d				"
-formatoQueso: .asciz "\nQueso				Q.35			%d				"
-formatoPan: .asciz "\nPan				Q.4			%d				"
-formatoJalea: .asciz "\nJalea			Q.26		%d				"
-formatoYogurt: .asciz "\nYogurt				Q.8			%d				"
-formatoManzana: .asciz "\nManzana				Q.19			%d				"
-formatoTotal: .asciz "\n															Total a pagar: %d"
+formatoLeche: .asciz "Leche			Q.18		%d			"
+formatoGalleta: .asciz "\nGalletas		Q.25		%d			"
+formatoMantequilla: .asciz "\nMantequilla		Q.10		%d			"
+formatoQueso: .asciz "\nQueso			Q.35		%d			"
+formatoPan: .asciz "\nPan			Q.4		%d			"
+formatoJalea: .asciz "\nJalea			Q.26		%d			"
+formatoYogurt: .asciz "\nYogurt			Q.8		%d			"
+formatoManzana: .asciz "\nManzana			Q.19		%d			"
+formatoTotal: .asciz "\n\nTotal a pagar: %d\n"
 
 .text
 .align 2
@@ -150,13 +150,54 @@ opLeche:
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
-	
-	ldr r0, =formatoNum
-	ldr r1, =cantidad
-	ldr r1, [r1]
-	bl printf
 
 	ldr r5, =cantidad_Leche
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	
+	
+	cmp r5, r1
+	blt Error
+	
+	
+	ldr r5, =cantidad_Leche
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Leche
+	str r1, [r5]
+
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	ldr r6, =cantidad_Solicitada_Leche
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Leche
+	str r6,[r8]
+
+	ldr r7, =precio_Leche
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
+
+	b menu
+
+opGalleta:
+	ldr r0, =formatoAlimento
+	bl puts
+
+	ldr r0, =formatoNum
+	ldr r1, =cantidad
+	bl scanf
+
+	ldr r5, =cantidad_Galletas
 	ldr r5, [r5]
 	ldr r1, =cantidad
 	ldr r1, [r1]
@@ -164,169 +205,303 @@ opLeche:
 	cmp r5, r1
 	blt Error
 	
+	ldr r5, =cantidad_Galletas
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
 	sub r5, r1
+	ldr r5, =cantidad_Galletas
+	str r1, [r5]
+
 	
 	ldr r1, =cantidad
 	ldr r1, [r1]
-	ldr r7, =precio_Leche
-	ldr r7, [r7]
-	ldr r6, =cantidad_Solicitada_Leche
+	ldr r6, =cantidad_Solicitada_Galletas
 	ldr r6, [r6]
 	add r6, r1
+	ldr r8, =cantidad_Solicitada_Galletas
+	str r6,[r8]
 
-
-	b suma
-
-suma:
-	mul r10, r6, r7
+	ldr r7, =precio_Galletas
+	ldr r7, [r7]
 	ldr r12, =total
 	ldr r12,[r12]
+	mul r10, r6, r7
 	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
 
 	b menu
 
-opGalleta:
-	ldr r0, =formatoAlimento
-	bl puts	
-
-	ldr r0, =formatoNum
-	ldr r1, =cantidad
-	bl scanf
-
-	ldr r5, =cantidad_Galletas
-
-	cmp r5,r1
-	blt Error
-
-	ldr r1, =cantidad
-	ldr r7, =precio_Galletas
-	ldr r6, =cantidad_Solicitada_Galletas
-	mul r6, r1, r7
-
-	b suma
-
 opMantequilla:
 	ldr r0, =formatoAlimento
-	bl puts	
+	bl puts
 
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
 
 	ldr r5, =cantidad_Mantequilla
-
-	cmp r5,r1
-	blt Error
-
+	ldr r5, [r5]
 	ldr r1, =cantidad
-	ldr r7, =precio_Mantequilla
-	ldr r6, =cantidad_Solicitada_Mantequilla
-	mul r6, r1, r7
+	ldr r1, [r1]
+	
+	cmp r5, r1
+	blt Error
+	
+	
+	ldr r5, =cantidad_Mantequilla
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Mantequilla
+	str r1, [r5]
 
-	b suma
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	ldr r6, =cantidad_Solicitada_Mantequilla
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Mantequilla
+	str r6,[r8]
+
+	ldr r7, =precio_Mantequilla
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
+
+	b menu
 
 opQueso:
 	ldr r0, =formatoAlimento
-	bl puts	
+	bl puts
 
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
 
 	ldr r5, =cantidad_Queso
-
-	cmp r5,r1
-	blt Error
-
+	ldr r5, [r5]
 	ldr r1, =cantidad
-	ldr r7, =precio_Queso
+	ldr r1, [r1]
+	
+	cmp r5, r1
+	blt Error
+	
+	
+	ldr r5, =cantidad_Queso
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Queso
+	str r1, [r5]
+
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
 	ldr r6, =cantidad_Solicitada_Queso
-	mul r6, r1, r7
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Queso
+	str r6,[r8]
 
+	ldr r7, =precio_Queso
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
 
-	b suma
+	b menu
 
 opPan:
 	ldr r0, =formatoAlimento
-	bl puts	
+	bl puts
 
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
 
 	ldr r5, =cantidad_Pan
-
-	cmp r5,r1
-	blt Error
-
+	ldr r5, [r5]
 	ldr r1, =cantidad
-	ldr r7, =precio_Pan
-	ldr r6, =cantidad_Solicitada_Pan
-	mul r6, r1, r7
+	ldr r1, [r1]
+	
+	cmp r5, r1
+	blt Error
+	
+	
+	ldr r5, =cantidad_Pan
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Pan
+	str r1, [r5]
 
-	b suma
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	ldr r6, =cantidad_Solicitada_Pan
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Pan
+	str r6,[r8]
+
+	ldr r7, =precio_Pan
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
+
+	b menu
 
 opJalea:
 	ldr r0, =formatoAlimento
-	bl puts	
+	bl puts
 
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
 
 	ldr r5, =cantidad_Jalea
-
-	cmp r5,r1
-	blt Error
-
+	ldr r5, [r5]
 	ldr r1, =cantidad
-	ldr r7, =precio_Jalea
-	ldr r6, =cantidad_Solicitada_Jalea
-	mul r6, r1, r7
+	ldr r1, [r1]
+	
+	cmp r5, r1
+	blt Error
+	
+	
+	ldr r5, =cantidad_Jalea
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Jalea
+	str r1, [r5]
 
-	b suma
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	ldr r6, =cantidad_Solicitada_Jalea
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Jalea
+	str r6,[r8]
+
+	ldr r7, =precio_Jalea
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
+
+	b menu
 
 opYogurt:
 	ldr r0, =formatoAlimento
-	bl puts	
+	bl puts
 
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
 
 	ldr r5, =cantidad_Yogurt
-
-	cmp r5,r1
-	blt Error
-
+	ldr r5, [r5]
 	ldr r1, =cantidad
-	ldr r7, =precio_Yogurt
+	ldr r1, [r1]
+	
+	cmp r5, r1
+	blt Error
+	
+
+	ldr r5, =cantidad_Yogurt
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Yogurt
+	str r1, [r5]
+
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
 	ldr r6, =cantidad_Solicitada_Yogurt
-	mul r6, r1, r7
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Yogurt
+	str r6,[r8]
 
+	ldr r7, =precio_Yogurt
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
 
-	b suma
+	b menu
 
 opManzana:
 	ldr r0, =formatoAlimento
-	bl puts	
+	bl puts
 
 	ldr r0, =formatoNum
 	ldr r1, =cantidad
 	bl scanf
 
 	ldr r5, =cantidad_Manzana
-
-	cmp r5,r1
-	blt Error
-
+	ldr r5, [r5]
 	ldr r1, =cantidad
-	ldr r7, =precio_Manzana
+	ldr r1, [r1]
+	
+	cmp r5, r1
+	blt Error
+	
+	
+	ldr r5, =cantidad_Manzana
+	ldr r5, [r5]
+	ldr r1, =cantidad
+	ldr r1, [r1]
+	sub r5, r1
+	ldr r5, =cantidad_Manzana
+	str r1, [r5]
+
+	
+	ldr r1, =cantidad
+	ldr r1, [r1]
 	ldr r6, =cantidad_Solicitada_Manzana
-	mul r6, r1, r7
+	ldr r6, [r6]
+	add r6, r1
+	ldr r8, =cantidad_Solicitada_Manzana
+	str r6,[r8]
 
+	ldr r7, =precio_Leche
+	ldr r7, [r7]
+	ldr r12, =total
+	ldr r12,[r12]
+	mul r10, r6, r7
+	add r12,r10
+	ldr r11, =total
+	str r12, [r11]
 
-	b suma
+	b menu
 
 Error:
 	ldr r0, =formatoError
@@ -353,82 +528,113 @@ salir:
 	ldr r0, =formatoNum
 	ldr r2, =precio_Leche
 	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Leche
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Galletas
 	ldr r0, =formatoGalleta
 	ldr r1, =cantidad_Solicitada_Galletas
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Galletas
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Galletas
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Mantequilla
 	ldr r0, =formatoMantequilla
 	ldr r1, =cantidad_Solicitada_Mantequilla
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Mantequilla
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Mantequilla
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Queso
 	ldr r0, =formatoQueso
 	ldr r1, =cantidad_Solicitada_Queso
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Queso
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Queso
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Pan
 	ldr r0, =formatoPan
 	ldr r1, =cantidad_Solicitada_Pan
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Pan
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Pan
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Jalea 
 	ldr r0, =formatoJalea
 	ldr r1, =cantidad_Solicitada_Jalea
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Jalea
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Jalea
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Yogurt
 	ldr r0, =formatoYogurt
 	ldr r1, =cantidad_Solicitada_Yogurt
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Yogurt
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Yogurt
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Ciclo de Manzana
 	ldr r0, =formatoManzana
 	ldr r1, =cantidad_Solicitada_Manzana
+	ldr r1, [r1]
 	bl printf
 
 	ldr r0, =formatoNum
 	ldr r2, =precio_Manzana
+	ldr r2, [r2]
+	ldr r1, =cantidad_Solicitada_Manzana
+	ldr r1, [r1]
 	mul r1, r1, r2
 	bl printf
 
 	@@ Total
 	ldr r0, =formatoTotal
 	ldr r1, =total
+	ldr r1, [r1]
 	bl printf
 
 
